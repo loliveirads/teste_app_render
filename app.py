d24b358329984611981274901568199175d8e8bc
loliveirads <119435421+loliveirads@users.noreply.github.com>
2023-01-08 19:37:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY],
                meta_tags=[{'name': 'viewport',
-                           'content': 'width=device-width,initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}]))
+                           'content': 'width=device-width,initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
 server = app.server
 
 
